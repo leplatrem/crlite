5e6d3d803ae0107a8e2b23c3a8ad10494c9c3be2
Dana Keeler <dkeeler@mozilla.com>
2021-04-28 15:56:03 -0700

    remove unused StorageBackend from FilesystemDatabase

--- go/cmd/aggregate-crls/aggregate-crls.go
+++ go/cmd/aggregate-crls/aggregate-crls.go
@@ -544,7 +544,7 @@ func checkPathArg(strObj string, confOptionName string, ctconfig *config.CTConfi
 func main() {
 	ctconfig.Init()
 	ctx, cancel := context.WithCancel(context.Background())
-	storageDB, remoteCache, _ := engine.GetConfiguredStorage(ctx, ctconfig)
+	storageDB, remoteCache := engine.GetConfiguredStorage(ctx, ctconfig)
 	defer glog.Flush()
 
 	checkPathArg(*revokedpath, "revokedpath", ctconfig)
